234f4408a00bcaa5dcb374ccb425f959a714c349
unknown <saifeel.momin@utexas.edu>
2020-03-01 05:01:51 -0600

    updated flask app and landing page

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from flask_pymongo import PyMongo
 from mars_scraper import scraper
 
+
 app = Flask(__name__)
 
 app = Flask(__name__)
@@ -15,32 +16,40 @@ mongo = PyMongo(app)
 @app.before_first_request
 def init_app():
     data = scraper()
+    mongo.db.mars_info.drop()
     collection = mongo.db.mars_info
     collection.insert_one(data)
 
-@app.route("/scrape")
+@app.route("/scrape", methods=['GET', 'POST'])
 def data_scrape():
-    data = scraper()
-    collection = mongo.db.mars_info
-    collection.insert_one(data)
+    if request.method == "POST":        
+        data = scraper()
+        mongo.db.mars_info.drop()
+        collection = mongo.db.mars_info
+        collection.insert_one(data)
+        # db_size = mongo.db.mars_info.count()
+        page_data = mongo.db.mars_info.find()[0]
+        return render_template("index.html", news_title=page_data['news'][1], news_p = page_data['news'][1], featured_img = page_data['img'], 
+                                facts = page_data['facts'], weather = page_data['weather'])
+    else: 
+        db_size = mongo.db.mars_info.count()
+        page_data = mongo.db.mars_info.find()[db_size-1]
+        return render_template("index.html", news_title=page_data['news'][1], news_p = page_data['news'][1], featured_img = page_data['img'], 
+                                facts = page_data['facts'], weather = page_data['weather'])
 
+##################################################  USE SELENIUM TO GET NASA NEWS INFO????    ###################################################################
 @app.route("/")
 def landing():
+    # db_size = mongo.db.mars_info.count()
     page_data = mongo.db.mars_info.find()[0]
-    # news_title = page_data['news'][0]
-    # news_p = page_data['news'][1]
-    # featured_image_url = page_data['img']
-    # weather = page_data['weather']
-    # facts = page_data['facts']
+
     # hemi1 = page_data['hemisphere_data']['hemi1']
     # hemi2 = page_data['hemisphere_data']['hemi2']
     # hemi3 = page_data['hemisphere_data']['hemi3']
     # hemi4 = page_data['hemisphere_data']['hemi4']
 
     return render_template("index.html", news_title=page_data['news'][1], news_p = page_data['news'][1], 
-                            featured_img = page_data['img'], facts = page_data['facts'], weather = page_data['weather'],
-                            hemi1 = page_data['hemisphere_data']['hemi1'], hemi2 = page_data['hemisphere_data']['hemi2'],
-                             hemi3 = page_data['hemisphere_data']['hemi3'], hemi4 = page_data['hemisphere_data']['hemi4'])
+                            featured_img = page_data['img'], facts = page_data['facts'], weather = page_data['weather'])
 
 # @app.route("/")
 # def hemispheres():
